e065dd9ff56e7e879cc4bc1dccbbfad2035c3926
Ligiane Vieira <ligiane_vieira@msn.com>
2022-01-22 21:36:39 -0300

    swagger

--- projeto/src/main/java/com/dio/Application.java
+++ projeto/src/main/java/com/dio/Application.java
@@ -3,18 +3,6 @@ package com.dio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-
-;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-
 
 
 @EnableFeignClients
@@ -26,15 +14,3 @@ public class Application {
 	}
 
 }
-@Configuration
-@EnableSwagger2
-class SwaggerConfig {
-	@Bean
-	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2)
-				.select()
-				.apis(RequestHandlerSelectors.any())
-				.paths(PathSelectors.any())
-				.build();
-	}
-	}
